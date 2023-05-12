5b1d747adb172761553872d55f897ff440a5a395
mdianeivanova <80835955+mdianeivanova@users.noreply.github.com>
2023-05-11 20:18:18 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,6 +5,9 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const PORT = 3000;
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.use(bodyParser.json());
 
 const uri = 'mongodb://localhost:27017';
